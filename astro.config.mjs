1c074ecf2cb96cd83ae4c918b756b5b1e4398b24
Jonathan Trowe <deine-mail@example.com>
2026-01-30 21:54:53 +0100

    Add PWA manifest and service worker

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,17 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 import vercel from "@astrojs/vercel/serverless";
+import { VitePWA } from "@vite-pwa/astro";
 
 export default defineConfig({
   output: "server",
   adapter: vercel(),
+  integrations: [
+    VitePWA({
+      registerType: "autoUpdate",
+      manifest: true,
+      workbox: {
+        globPatterns: ["**/*.{js,css,html,ico,png,svg}"]
+      }
+    })
+  ]
 });
